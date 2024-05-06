2d945431e93e2b4a6a08388dd609970cd51c224c
Zakariyah Ali <zakariyahali100@gmail.com>
2024-05-06 16:13:37 +0000

    error fixed

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,30 +1,18 @@
+#!/usr/bin/env python3
+""" Basic syntax await async """
 import asyncio
 import random
 
 
-async def wait_random(max_delay=10):
+async def wait_random(max_delay: int = 10) -> float:
     """
-    This asynchronous coroutine waits for a random delay
-    between 0 and max_delay (inclusive) seconds
-    and returns the delay value.
+        Args:
+            max_delay: max wait
 
-    Args:
-        max_delay (int, optional): The maximum delay
-        in seconds. Defaults to 10.
-
-    Returns:
-        float: The random delay between 0 and max_delay (inclusive) seconds.
+        Return:
+            float time random
     """
-    delay = random.uniform(
-        0, max_delay)  # Generate random delay between 0 and max_delay
-    await asyncio.sleep(delay)  # Wait for the random delay asynchronously
+    delay: float = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+
     return delay
-
-
-async def main():
-    # Wait for a random delay between 0 and 5 seconds
-    delay = await wait_random(5)
-    print(f"Waited for {delay:.2f} seconds")
-
-if __name__ == "__main__":
-    asyncio.run(main())
